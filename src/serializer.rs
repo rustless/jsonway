1a2e98611b1b37d896a5daae728fb204e629b70e
Stanislav Panferov <fnight.m@gmail.com>
2014-10-14 19:43:13 +0400

    feat: add `ObjectSerializer`

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,3 +1,5 @@
+use serialize::json::Json;
+
 use object_builder::ObjectBuilder;
 
 /// Provides functionality to create custom JSON presenters for your structs.
@@ -11,9 +13,55 @@ use object_builder::ObjectBuilder;
 ///     name: String
 /// }
 ///
+/// struct JediSerializer<'a> {
+///     jedi: &'a Jedi
+/// }
+///
+/// impl<'a> Serializer for JediSerializer<'a> {
+///     fn root(&self) -> Option<&str> { Some("jedi") }
+///     fn build(&self, json: &mut ObjectBuilder) {
+///         json.set("name", self.jedi.name.to_string());
+///     }
+/// }
+///
+/// let jedi = Jedi { name: "Saes Rrogon".to_string() };
+/// let json = JediSerializer{jedi: &jedi}.serialize();
+/// ```
+pub trait Serializer {
+
+    fn build(&self, &mut ObjectBuilder);
+    
+    #[inline]
+    fn root(&self) -> Option<&str> {
+        None
+    }
+
+    fn serialize(&mut self) -> Json {
+        let mut bldr = ObjectBuilder::new();
+        let root = self.root();
+        if root.is_some() {
+            bldr.root(root.unwrap())
+        }
+        self.build(&mut bldr);
+
+        bldr.unwrap()
+    }
+}
+
+/// Provides functionality to create custom JSON presenters for your structs.
+/// 
+/// ## Example 
+/// 
+/// ```
+/// use jsonway::{ObjectBuilder, ObjectSerializer};
+///
+/// struct Jedi {
+///     name: String
+/// }
+///
 /// struct JediSerializer;
 ///
-/// impl Serializer<Jedi> for JediSerializer {
+/// impl ObjectSerializer<Jedi> for JediSerializer {
 ///     fn root(&self) -> Option<&str> { Some("jedi") }
 ///     fn build(&self, jedi: &Jedi, json: &mut ObjectBuilder) {
 ///         json.set("name", jedi.name.to_string());
@@ -21,9 +69,9 @@ use object_builder::ObjectBuilder;
 /// }
 ///
 /// let jedi = Jedi { name: "Saes Rrogon".to_string() };
-/// let json = JediSerializer.serialize(&jedi).unwrap();
+/// let json = JediSerializer.serialize(&jedi);
 /// ```
-pub trait Serializer<T> {
+pub trait ObjectSerializer<T> {
 
     fn build(&self, &T, &mut ObjectBuilder);
     
@@ -32,14 +80,13 @@ pub trait Serializer<T> {
         None
     }
 
-    fn serialize(&mut self, obj: &T) -> ObjectBuilder {
+    fn serialize(&mut self, obj: &T) -> Json {
         let mut bldr = ObjectBuilder::new();
         let root = self.root();
         if root.is_some() {
             bldr.root(root.unwrap())
         }
         self.build(obj, &mut bldr);
-
-        bldr
+        bldr.unwrap()
     }
 }
