6c197905a3edabb8ccbe75d8bd124daf56f439a2
Stanislav Panferov <fnight.m@gmail.com>
2014-11-23 10:58:09 +0300

    refactor: rename list to array to align with new Rust

--- src/array_builder.rs
+++ src/array_builder.rs
@@ -1,22 +1,22 @@
 use serialize::json;
-use serialize::json::{JsonList, Json, ToJson};
+use serialize::json::{JsonArray, Json, ToJson};
 use std::collections::TreeMap;
 
 use object_builder::ObjectBuilder;
 
-pub struct ListBuilder {
-    list: JsonList,
+pub struct ArrayBuilder {
+    array: JsonArray,
     pub null: bool,
     pub skip: bool,
     pub root: Option<String>
 }
 
-/// Use ListBuilder to produce JSON arrays
-impl ListBuilder {
+/// Use ArrayBuilder to produce JSON arrays
+impl ArrayBuilder {
 
-    pub fn new() -> ListBuilder {
-        ListBuilder { 
-            list: vec![], 
+    pub fn new() -> ArrayBuilder {
+        ArrayBuilder { 
+            array: vec![], 
             null: false,
             skip: false,
             root: None
@@ -24,10 +24,10 @@ impl ListBuilder {
     }
 
     /// Initialize builder with initial value.
-    pub fn from_json(list: Json) -> Option<ListBuilder> {
-        match list {
-            json::List(list) => Some(ListBuilder { 
-                list: list, 
+    pub fn from_json(array: Json) -> Option<ArrayBuilder> {
+        match array {
+            json::Array(array) => Some(ArrayBuilder { 
+                array: array, 
                 null: false,
                 skip: false,
                 root: None
@@ -36,22 +36,22 @@ impl ListBuilder {
         }
     }
 
-    /// Create new ListBuilder, pass it to closure as mutable ref and return.
-    pub fn build(builder: |&mut ListBuilder|) -> ListBuilder {
-        let mut bldr = ListBuilder::new();
+    /// Create new ArrayBuilder, pass it to closure as mutable ref and return.
+    pub fn build(builder: |&mut ArrayBuilder|) -> ArrayBuilder {
+        let mut bldr = ArrayBuilder::new();
         builder(&mut bldr);  
         
         bldr 
     }
 
-    /// Push JSON value to list.
+    /// Push JSON value to array.
     pub fn push_json(&mut self, value: Json) {
-        self.list.push(value);
+        self.array.push(value);
     }
 
-    /// Create new list and push it.
-    pub fn list(&mut self, builder: |&mut ListBuilder|) {
-        self.push(ListBuilder::build(builder).unwrap());
+    /// Create new array and push it.
+    pub fn array(&mut self, builder: |&mut ArrayBuilder|) {
+        self.push(ArrayBuilder::build(builder).unwrap());
     }
 
     /// Create new object and push it
@@ -59,13 +59,13 @@ impl ListBuilder {
         self.push(ObjectBuilder::build(builder).unwrap());
     }
 
-    /// It you call `null`, this list will be converted to null when converting
+    /// It you call `null`, this array will be converted to null when converting
     /// to raw JSON value.
     pub fn null(&mut self) {
         self.null = true;
     }
 
-    /// It you call `skip`, this list will be skipped.
+    /// It you call `skip`, this array will be skipped.
     pub fn skip(&mut self) {
         self.skip = true;
     }
@@ -98,20 +98,20 @@ impl ListBuilder {
         if self.null {
             json::Null
         } else {
-            json::List(self.list)
+            json::Array(self.array)
         }
     }
 }
 
-impl<T: ToJson> ListBuilder {
-    /// Push to list something that can be converted to JSON.
+impl<T: ToJson> ArrayBuilder {
+    /// Push to array something that can be converted to JSON.
     pub fn push(&mut self, value: T) {
         self.push_json(value.to_json());
     }
 }
 
-impl<A, T: Iterator<A>> ListBuilder {
-    /// Fill this list by objects builded from iterator.
+impl<A, T: Iterator<A>> ArrayBuilder {
+    /// Fill this array by objects builded from iterator.
     pub fn objects(&mut self, iter: &mut T, func: |A, &mut ObjectBuilder|) {
         for a in *iter {
             let mut bldr = ObjectBuilder::new();
@@ -122,10 +122,10 @@ impl<A, T: Iterator<A>> ListBuilder {
         }
     }
 
-    // Fill this list by lists builded from iterator.
-    pub fn lists(&mut self, iter: &mut T, func: |A, &mut ListBuilder|) {
+    // Fill this array by arrays builded from iterator.
+    pub fn arrays(&mut self, iter: &mut T, func: |A, &mut ArrayBuilder|) {
         for a in *iter {
-            let mut bldr = ListBuilder::new();
+            let mut bldr = ArrayBuilder::new();
             func(a, &mut bldr);
             if !bldr.skip {
                 self.push(bldr.unwrap())
@@ -133,7 +133,7 @@ impl<A, T: Iterator<A>> ListBuilder {
         }
     }
 
-    /// Fill this list by JSON values builded from iterator.
+    /// Fill this array by JSON values builded from iterator.
     pub fn map(&mut self, iter: &mut T, func: |A| -> Json) {
         for a in *iter {
             self.push(func(a))      
@@ -141,9 +141,9 @@ impl<A, T: Iterator<A>> ListBuilder {
     }
 }
 
-impl ToJson for ListBuilder {
+impl ToJson for ArrayBuilder {
     /// Copy self to new JSON instance.
     fn to_json(&self) -> Json {
-         if self.null { json::Null } else { self.list.to_json() }
+         if self.null { json::Null } else { self.array.to_json() }
     }
 }
