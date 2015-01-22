29eec1d749d573ef6391b062c40ee5f1a1031029
Stanislav Panferov <fnight.m@gmail.com>
2015-01-22 16:01:39 +0300

    doc(*): rebuilding docs 2015-01-22T13:01:39Z

---
+++ doc/implementors/serialize/json/trait.ToJson.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['jsonway'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/json/trait.ToJson.html' title='serialize::json::ToJson'>ToJson</a> for <a class='struct' href='jsonway/list_builder/struct.ListBuilder.html' title='jsonway::list_builder::ListBuilder'>ListBuilder</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/json/trait.ToJson.html' title='serialize::json::ToJson'>ToJson</a> for <a class='struct' href='jsonway/object_builder/struct.ObjectBuilder.html' title='jsonway::object_builder::ObjectBuilder'>ObjectBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
